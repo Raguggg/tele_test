9e35287270bb9fb03a05d42852ddb589b235fb35
Ragu <88898517+Raguggg@users.noreply.github.com>
2022-12-09 17:41:27 +0530

    Create main.py

--- main.py
+++ main.py
@@ -6,8 +6,20 @@ API_HASH = "faa0e3b6d30779426d833dec02ab70d9"
 TOKEN = "1969982366:AAHoE4d2bnRem_Ng8pL694z_jHQAXMJB6LA"
 
 app = Client("tagremover", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
+
+def progress_show(percent=0, width=40):
+    left = width * percent // 100
+    right = width - left
+
+    tags = "#" * left
+    spaces = " " * right
+    percents = f"{percent:.0f}%"
+
+    print("\r[", tags, spaces, "]", percents, sep="", end="", flush=True)
 async def progress(current, total):
-    print(f"{current * 100 / total:.1f}%")
+    progress_show(round(current * 100 / total))
+    # print(f"{current * 100 / total:.1f}%")
+
 @app.on_message(filters.private & filters.text | filters.media)
 async def tag(client, message):
     print(message,sep=" <---> ")
@@ -16,5 +28,5 @@ async def tag(client, message):
         text=str(message),
         reply_to_message_id=message.id
     )
-    # await app.download_media(message, progress=progress,file_name="hello.mp4")
+    await app.download_media(message, progress=progress,file_name="/content/drive/MyDrive/telegram/download/")
 app.run()
